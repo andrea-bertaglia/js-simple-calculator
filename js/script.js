392f93c55bca3b95e956c248c09c262548a4ee30
andrea-bertaglia <andreabertaglia@gmail.com>
2024-04-09 17:34:54 +0200

    feat: refactoring script.js event listener

--- js/script.js
+++ js/script.js
@@ -1,57 +1,42 @@
 // test
 console.log('Test link script.js');
 
+// NUMERI
 // prelevo i numeri
-const btnListenerZero = document.getElementById("0");
-const btnListenerOne = document.getElementById("1");
-const btnListenerTwo = document.getElementById("2");
-const btnListenerThree = document.getElementById("3");
-const btnListenerFour = document.getElementById("4");
-const btnListenerFive = document.getElementById("5");
-const btnListenerSix = document.getElementById("6");
-const btnListenerSeven = document.getElementById("7");
-const btnListenerEight = document.getElementById("8");
-const btnListenerNine = document.getElementById("9");
+const btnListenerElems = document.querySelectorAll(".btn-number");
+// associo event listener ai numeri
+for(let i = 0; i < btnListenerElems.length; i++) {
+    btnListenerElems[i].addEventListener("click", getNum)
+}
 
+// OPERATORI
 // prelevo gli operatori
-const btnListenerPlus = document.getElementById("plus");
-const btnListenerMinus = document.getElementById("minus");
-const btnListenerTimes = document.getElementById("times");
-const btnListenerDivide = document.getElementById("divide");
+const btnListenerOp = document.querySelectorAll(".btn-op")
+// associo event listener agli operatori
+for(let i = 0; i < btnListenerOp.length; i++) {
+    btnListenerOp[i].addEventListener("click", getOperation);
+}
 
+// DISPLAY
 // prelevo l'elemento del display
-const btnListenerDisplay = document.getElementById("total");
+const btnListenerDisplay = document.getElementById("display");
 
-// prelevo l'elemento "canc"
-const btnListenerCancel = document.getElementById("cancel");
 
+// TASTO TOTALE
 // prelevo il tasto del totale
 const btnListenerEquals = document.getElementById("equals")
-
-// associo event listener ai numeri
-btnListenerZero.addEventListener("click", getNum);
-btnListenerOne.addEventListener("click", getNum);
-btnListenerTwo.addEventListener("click", getNum);
-btnListenerThree.addEventListener("click", getNum);
-btnListenerFour.addEventListener("click", getNum);
-btnListenerFive.addEventListener("click", getNum);
-btnListenerSix.addEventListener("click", getNum);
-btnListenerSeven.addEventListener("click", getNum);
-btnListenerEight.addEventListener("click", getNum);
-btnListenerNine.addEventListener("click", getNum);
-
-// associo event listener agli operatori
-btnListenerPlus.addEventListener("click", getOperation);
-btnListenerMinus.addEventListener("click", getOperation);
-btnListenerTimes.addEventListener("click", getOperation);
-btnListenerDivide.addEventListener("click", getOperation);
-
 // associo event listener al tasto del totale
 btnListenerEquals.addEventListener("click", getTotal);
 
+// TASTO CANC
+// prelevo l'elemento "canc"
+const btnListenerCancel = document.getElementById("cancel");
 // associo event listener al tasto cancel
 btnListenerCancel.addEventListener("click", cancelDisplay)
 
+
+
+// DICHIARAZIONE DELLE VARIABILI
 let NumDisplay = "";
 let operationSign = "";
 let firstNumber = 0;
